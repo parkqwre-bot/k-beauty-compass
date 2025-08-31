f1203349db7851bcbaa751afe4ad567b02cbcc00
김영륜 <parkqwre@gmail.com>
2025-08-31 19:03:15 +0900

    fix: Align product data model      with service logic

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,22 +1,31 @@
 class Product {
+  final int id;
   final String name;
-  final String recommendation;
   final String imageUrl;
-  final String affiliateUrl;
+  final String affiliateUrlCoupang;
+  final String affiliateUrlAmazon;
+  final String recommendation;
+  final Map<String, dynamic> attributes;
 
   Product({
+    required this.id,
     required this.name,
-    required this.recommendation,
     required this.imageUrl,
-    required this.affiliateUrl,
+    required this.affiliateUrlCoupang,
+    required this.affiliateUrlAmazon,
+    required this.recommendation,
+    required this.attributes,
   });
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
-      name: json['name'] ?? '',
-      recommendation: json['recommendation'] ?? '',
-      imageUrl: json['imageUrl'] ?? '',
-      affiliateUrl: json['affiliateUrl'] ?? '',
+      id: json['id'],
+      name: json['name'],
+      imageUrl: json['imageUrl'],
+      affiliateUrlCoupang: json['affiliateUrlCoupang'],
+      affiliateUrlAmazon: json['affiliateUrlAmazon'],
+      recommendation: json['recommendation'],
+      attributes: json['attributes'],
     );
   }
 }
